7deade7e30a3c61d9f31f59165c3f6200d3d0e1c
prajwalmbmsce <prajwalm.ec23@bmsce.ac.in>
2026-02-04 20:13:55 +0530

    Create siso.sv

---
+++ EXP 7/siso.sv
@@ -0,0 +1,14 @@
+
+// -----------------------------------------------------------------------------
+// File        : siso.sv
+// Author      : Prajwal M (1BM23EC187)
+// Created     : 2026-01-26
+// Module      : siso
+// Project     : SystemVerilog and Verification (23EC6PE2SV),
+//               Faculty: Prof. Ajaykumar Devarapalli
+// Description : SISO shift register for serial data shifting.
+// ----------------------------------------------------------------------------- 
+module siso(input clk , si , output so);logic [3:0] q;
+  assign so = q[3];
+  always_ff @(posedge clk) q <= {q[2:0] , si };
+endmodule
