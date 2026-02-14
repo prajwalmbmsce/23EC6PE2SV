e58d1f921eed56d93b6b36eab719f15d6c29cdfa
prajwalmbmsce <prajwalm.ec23@bmsce.ac.in>
2026-02-14 12:56:24 +0530

    Create packet_class.sv

---
+++ AAT/TASK 4/packet_class.sv
@@ -0,0 +1,11 @@
+// -----------------------------------------------------------------------------
+// File        : packet_class.sv
+// Author      : Prajwal M (1BM23EC187)
+// Created     : 2026-01-29
+// Module      : packet_class
+// Project     : SystemVerilog and Verification (23EC6PE2SV),
+//               Faculty: Prof. Ajaykumar Devarapalli
+// Description : a dummy module for the functionality.
+// ----------------------------------------------------------------------------- 
+module packet_class;
+endmodule
